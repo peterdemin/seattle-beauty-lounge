e373777b5116a8d778f51423bc68e2d80d1a8a88
Peter Demin <peterdemin@gmail.com>
2024-12-28 20:51:36 -0800

    Deploy static to staging

--- tools/deploy_staging.sh
+++ tools/deploy_staging.sh
@@ -3,5 +3,9 @@
 set -e -o pipefail
 
 git archive --format=tar HEAD api -o caprover-api.tar
-caprover deploy -t caprover-api.tar -a api -u staging.seattle-beauty-lounge.com -d
+caprover deploy -t caprover-api.tar -a api -u staging.seattle-beauty-lounge.com
 rm -rf caprover-api.tar
+
+tar cf caprover-static.tar public captain-definition Dockerfile
+caprover deploy -t caprover-static.tar -a static -u staging.seattle-beauty-lounge.com
+rm -rf caprover-static.tar
