85599814f5423f9c35bdb13be9fe03f2f44f343f
Peter Demin <peterdemin@gmail.com>
2024-12-29 19:38:00 -0800

    Automate caprover setup

--- tools/bootstrap_staging.sh
+++ tools/bootstrap_staging.sh
@@ -4,18 +4,9 @@ set -e -o pipefail
 scp tools/init_staging.sh staging.seattle-beauty-lounge.com:
 ssh staging.seattle-beauty-lounge.com -- "chmod +x init_staging.sh && sudo ./init_staging.sh"
 
-cat <<EOF
-Replicate this CapRover server setup (Password in the password manager):
-
-? have you already started CapRover container on your server? Yes
-? IP address of your server: 35.185.245.45
-? CapRover server root domain: staging.seattle-beauty-lounge.com
-? new CapRover password (min 8 characters): [hidden]
-? enter new CapRover password again: [hidden]
-? "valid" email address to get certificate and enable HTTPS: peter@seattle-beauty-lounge.com
-? CapRover machine name, with whom the login credentials are stored locally: staging
-
-CapRover server setup completed: it is available as staging at https://captain.staging.seattle-beauty-lounge.com
-EOF
-
-caprover serversetup
+caprover setup -y \
+    -r staging.seattle-beauty-lounge.com \
+    -e peter@seattle-beauty-lounge.com \
+    -p captain42 \
+    -w "${CAPROVER_PASSWORD}" \
+    -n staging
