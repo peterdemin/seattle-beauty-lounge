48ef00a606f5ede686286778b6ebf44800b09abe
Peter Demin <peterdemin@gmail.com>
2025-01-02 11:23:28 -0800

    Add Sentry to frontend

--- source/scripts/BookingWizard.jsx
+++ source/scripts/BookingWizard.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect, StrictMode } from "react";
 import { DayPicker } from "react-day-picker";
 import ReactDOM from "react-dom/client";
 import "/rdp-style.css";
+import * as Sentry from "@sentry/react";
 import { CheckoutProvider } from "@stripe/react-stripe-js";
 import { PaymentElement } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
@@ -454,6 +455,21 @@ const CheckoutStep = ({ clientEmail, clientSecret, onConfirm, stripe }) => {
 	}
 	return null;
 };
+
+Sentry.init({
+	dsn: import.meta.env.VITE_SENTRY_DSN,
+	integrations: [
+		Sentry.browserTracingIntegration(),
+		Sentry.replayIntegration(),
+	],
+	// Tracing
+	tracesSampleRate: 1.0, //  Capture 100% of the transactions
+	// Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+	tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+	// Session Replay
+	replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+	replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
 
 ReactDOM.createRoot(document.getElementById("book")).render(
 	<StrictMode>
