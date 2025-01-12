f5cceb8e6ea141d9be18b776492641eb754470bc
Peter Demin <peterdemin@gmail.com>
2025-01-11 20:41:53 -0800

    Disable sentry plugin (too slow)

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,15 @@
-import { sentryVitePlugin } from "@sentry/vite-plugin";
 import react from "@vitejs/plugin-react";
+// import { sentryVitePlugin } from "@sentry/vite-plugin";
 // vite.config.js
 import { defineConfig } from "vite";
 
 export default defineConfig({
 	plugins: [
 		react(),
-		sentryVitePlugin({
-			org: "seattle-beauty-lounge",
-			project: "javascript",
-		}),
+		// sentryVitePlugin({
+		// 	org: "seattle-beauty-lounge",
+		// 	project: "javascript",
+		// }),
 	],
 
 	root: "source/scripts",
